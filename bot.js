ad01996df03a0a057ba5e16d934bfbdf62235e10
dR3am5t0rM <sizhe@hotmail.com>
2017-03-23 14:01:04 +0000

    Added youtube queue, need to refactor and fix features such as ~playing and limit queue size and youtube video length.

--- bot.js
+++ bot.js
@@ -1,10 +1,16 @@
 var fs = require("fs");
 var request = require("request");
+var ytdl = require("ytdl-core");
 var discord = require("discord.js");
 var bot = new discord.Client();
 
+var YT_QUEUE = [];
 var RADIO_CHANNELS = [];
+var CHANNELS_JOINED = [];
 var CONFIG_JSON = {};
+
+var playing_radio = true;
+
 const RADIO_LINK = "https://relay0.r-a-d.io/main.mp3";
 const API_LINK = "https://r-a-d.io/api";
 
@@ -15,7 +21,41 @@ const CONFIG = "config.json";
  * Stream RADIO_LINK to the given voice connection.
  */
 var play_radio = function(voice_connection) {
+    playing_radio = true;
     var dispatcher = voice_connection.playStream(request(RADIO_LINK), {seek: 0, volume: 1});
+}
+
+function play_link(stream) {
+    voice_connections = bot.voiceConnections.array();
+    var dispatcher;
+    for (var i = 0; i < voice_connections.length; i++) {
+        vc = voice_connections[i]
+        dispatcher = vc.playStream(stream, {seek: 0, volume: 2});
+    }
+
+    dispatcher.on("end", function() {
+        console.log("ended link");
+        play_queue();
+    });
+}
+
+function play_queue() {
+    console.log(YT_QUEUE);
+    playing_radio = false;
+    if (YT_QUEUE.length == 0) {
+        console.log("back to radio");
+        for (var i = 0; i < bot.voiceConnections.array().length; i++) {
+            play_radio(bot.voiceConnections.array()[i]);
+        }
+    }
+    else {
+        var link = YT_QUEUE.splice(0, 1)[0];
+        console.log(link);
+        var stream = ytdl(link, {filter: "audioonly"});
+
+        play_link(stream);
+    }
+
 }
 
 /**
@@ -46,6 +86,12 @@ function join_channel(channel) {
 function move(channel) {
     console.log("Moved to voice channel " + channel.name)
     channel.join();
+
+    // console.log(bot.voiceConnections.array().length);
+
+    for (var i = 0; i < bot.voiceConnections.array().length; i++) {
+        console.log(bot.voiceConnections.array()[i].channel);
+    }
 }
 
 
@@ -58,6 +104,15 @@ function add_channel(channel_id) {
 function move_to_channel(channel_id) {
     var channel_to_move = get_radio_channel(channel_id, bot.channels.array());
     move(channel_to_move);
+}
+
+function add_to_queue(link) {
+    console.log(YT_QUEUE);
+    YT_QUEUE.push(link);
+
+    if (playing_radio) {
+        play_queue();
+    }
 }
 
 
@@ -106,6 +161,8 @@ function send_dj(channel) {
     });
 }
 
+// function send_message(channel, msg)
+
 /* 
  * Bot event handlers 
  */
@@ -125,6 +182,11 @@ bot.on("message", function(msg) {
         case "~dj":
             send_dj(msg.channel);
             break;
+
+        case (msg.content.match(/~add\s(.*)/) || {}).input:
+            add_to_queue(msg.content.split(" ")[1]);
+            break;
+
         case (msg.content.match(/~join\s[0-9]*/) || {}).input:
             add_channel(msg.content.split(" ")[1]);
             break; 
@@ -133,6 +195,7 @@ bot.on("message", function(msg) {
             break;
 
     }
+    
 });
 
 bot.on("ready", function() {
